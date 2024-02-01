42910c0c3f10bba30c3fdbef560c60e71c53da96
GhanBuriGhan <moist_daddy@LAPTOP-C5EA37F4.localdomain>
2024-02-01 00:27:09 +0000

    removals

---
+++ README.md
@@ -0,0 +1,17 @@
+## Introduction
+
+The objective of this project is to learn the GraphQL query language, by creating your own profile page.
+
+## Running the program
+
+Go to https://moist-daddy.github.io./index.html
+
+Alternatively use Live Server on index. html
+
+##  Testing the program
+
+Audit can be found [here](https://github.com/01-edu/public/tree/master/subjects/graphql/audit)
+
+## Written by
+
+@GhanBuriGhan / Matis Altermann
--- charts.js
+++ charts.js
@@ -42,43 +42,41 @@ export function createSimplePieChart(a, b) {
   // Append labels to the slices
   slices.append('text')
     .attr('transform', (d) => `translate(${arc.centroid(d)})`)
-    .attr('dy', '0.35em') // Adjust vertical alignment
+    .attr('dy', '0.35em')
     .attr('text-anchor', 'middle')
     .text((d, i) => {
       return i === 0 ? 'Done' : 'Received';
     });
 }
 
-// charts.js
-
 export function createSimpleBarChart(a, b, c) {
   // Data for the bar chart
   const data = [a, b, c];
-  const labels = ['Go Piscine', 'JS Piscine', 'Div 1'];  // Add labels for each bar
+  const labels = ['Go Piscine', 'JS Piscine', 'Div 1'];
 
   // Size of the bar chart
   const width = 300;
   const height = 200;
   const barWidth = 80;
-  const padding = 20;  // Adjust padding between bars
+  const padding = 20;
 
   // Create a linear scale for y-axis
   const yScale = d3.scaleLinear()
-    .domain([0, d3.max(data) * 1.2])  // Adjust the domain to cover a bit more than the maximum value
-    .range([height, 0]);  // Range: height to 0 (top to bottom)
+    .domain([0, d3.max(data) * 1.2])
+    .range([height, 0]);
 
   // Create SVG container
   const svg = d3.select('#xp-bar-chart')
     .append('svg')
     .attr('width', width)
-    .attr('height', height + padding);  // Increase the height to accommodate labels
+    .attr('height', height + padding);
 
   // Create bars
   svg.selectAll('rect')
     .data(data)
     .enter()
     .append('rect')
-    .attr('x', (d, i) => i * (barWidth + padding))  // Adjusted padding between bars
+    .attr('x', (d, i) => i * (barWidth + padding))
     .attr('y', d => yScale(d))
     .attr('width', barWidth)
     .attr('height', d => height - yScale(d))
@@ -89,9 +87,9 @@ export function createSimpleBarChart(a, b, c) {
     .data(labels)
     .enter()
     .append('text')
-    .attr('x', (d, i) => i * (barWidth + padding) + barWidth / 2)  // Center the label under each bar
-    .attr('y', height + padding)  // Adjusted y-coordinate for labels
-    .attr('text-anchor', 'middle')  // Center the text horizontally
+    .attr('x', (d, i) => i * (barWidth + padding) + barWidth / 2)
+    .attr('y', height + padding)
+    .attr('text-anchor', 'middle')
     .text(d => d);
 }
 
